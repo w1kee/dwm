b9b95cd059395c36c440df16e2f106e69c1f49f2
Viktor Piacka <viktor@piacka.com>
2020-11-20 16:14:10 +0100

    patched in bar height

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
